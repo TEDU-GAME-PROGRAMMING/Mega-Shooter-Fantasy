a05a0ac45a59f55a0435e1a2e7fb20c85e7ab4f5
atincuzun <t.atincuzun@hotmail.com>
2022-03-22 23:29:20 +0300

    GameScene options menu integration to MainScene
    GameScene options menu integration to MainScene

--- Assets/Scripts/SettingsInGame.cs
+++ Assets/Scripts/SettingsInGame.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class SettingsInGame : MonoBehaviour
 {
+    public GameObject settingsCanvas;
+    public string settingCanvasName = "SettingsCanvas";
+    public bool display = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(settingsCanvas == null)
+        {
+            settingsCanvas = GameObject.Find(settingCanvasName);
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            display = !display;
+            settingsCanvas.SetActive(display);
+        }
+
     }
 
 }
